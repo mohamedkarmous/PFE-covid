66bbf72a95280f9e6696e454926aa37e6c00b7fd
mohamedkarmous <mohamedlahbibkarmous@gmail.com>
2021-03-15 19:24:50 +0100

    modified types.js

--- covid/frontend/src/actions/types.js
+++ covid/frontend/src/actions/types.js
@@ -1,3 +1,4 @@
+//user
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
@@ -8,6 +9,8 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT_FAIL = "LOGOUT_FAIL";
 export const LOGOUT = "LOGOUT";
+
+//patients
 export const GET_PATIENT = "GET_PATIENT";
 export const PATIENT_ERROR = "PATIENT_ERROR";
 export const GET_PATIENTS = "GET_PATIENTS";
