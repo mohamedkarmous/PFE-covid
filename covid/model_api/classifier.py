21eda43d311c0cc6d274b69f80fbdef4caeb905f
mohamedkarmous <mohamedlahbibkarmous@gmail.com>
2021-04-09 23:42:12 +0100

    added proba to model

--- covid/model_api/classifier.py
+++ covid/model_api/classifier.py
@@ -10,16 +10,17 @@ class _Classifier:
 
     def predict(self, list):
 
-        p = self.model.predict(list)
-        print("prediction : ", self._mapping[p[0]])
-        result = [self._mapping[p[0]]]
+        p = self.model.predict_proba(list)
+        print("prediction : ", p)
+        result = [self._mapping[np.argmax(
+            p)]+" "+str(round(max(p[0])*100))+"%"]
         return (result)
 
 
 def Classifier():
     if _Classifier._instance is None:
         _Classifier._instance = _Classifier()
-        with open('.\VotingClassifier.pkl', 'rb') as file:
+        with open('.\GradientBoostingClassifier.pkl', 'rb') as file:
             pickle_model = pickle.load(file)
         _Classifier.model = pickle_model
     return _Classifier._instance
